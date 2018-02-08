1b4952bc61f260b696e161cda4678bac8460f066
efran15 <35343406+efran15@users.noreply.github.com>
2018-02-08 11:51:33 -0600

    Updated

---
+++ JS/code-assignment-03.js
@@ -0,0 +1,6 @@
+let projectId = 1
+let projectTitle = "Earthquake Map"
+let thumbnail = true
+let projectTitles = ["Earthquake Map", "Map of Earthquakes of magnitude 4.5 or greater"]
+
+console.log(projectId)
