600d8cda658170df06a410eba98211728031a78e
efran15 <35343406+efran15@users.noreply.github.com>
2018-02-20 10:29:05 -0600

    updated

--- JS/code-assignment-03.js
+++ JS/code-assignment-03.js
@@ -1,6 +1,15 @@
+// assign values to a set of variables representing a project's metadata
 let projectId = 1
-let projectTitle = "Earthquake Map"
-let thumbnail = true
-let projectTitles = ["Earthquake Map", "Map of Earthquakes of magnitude 4.5 or greater"]
+let projectUrl = 'http://arcg.is/1KmqbS'
+let projectTitle = 'Map of Recent Earthquakes'
+let projectDesc = 'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 4.5 or greater that have occurred in the last 7 days. The dataset is streamed from the USGS.'
+let projectThumb = true
+let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
 
-console.log(projectId)
+// display messages in the browser's developer console, combining the values with text
+console.log('ID: ' + projectId)
+console.log('URL: ' + projectUrl)
+console.log('Title: ' + projectTitle)
+console.log('Description: ' + projectDesc)
+console.log('Has thumbnail? ' + projectThumb)
+console.log('Map extent: ' + projectMapExtent)
