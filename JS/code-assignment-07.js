3cc045da021c689ccddc6bcbb84e23df9e5b4f07
efran15 <35343406+efran15@users.noreply.github.com>
2018-03-20 11:42:14 -0500

    update

--- JS/code-assignment-07.js
+++ JS/code-assignment-07.js
@@ -50,14 +50,3 @@ for (let i=0; i < projectData.length; i++) {
   createTitle(projectData[i].title)
   createThumbnail(projectData[i].thumb, projectData[i].id)
 }
-
-function createTitle (title) {
-  console.log('title:' + title)
-}
-function createThumbnail (thumb, id) {
-  if (thumb === true) {
-    console.log('images/book-assignment-0' + id + '.PNG' )
-  } else {
-      console.log('images/no-image.PNG')
-  }
-}
---
+++ JS/functions.js
@@ -0,0 +1,10 @@
+function createTitle (title) {
+  console.log('title:' + title)
+}
+function createThumbnail (thumb, id) {
+  if (thumb === true) {
+    console.log('images/book-assignment-0' + id + '.PNG' )
+  } else {
+      console.log('images/no-image.PNG')
+  }
+}
