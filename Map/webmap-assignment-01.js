2eff09450605bfbe0cf94dc6bd6064deae77155d
efran15 <35343406+efran15@users.noreply.github.com>
2018-03-22 11:50:12 -0500

    update

---
+++ Map/webmap-assignment-01.js
@@ -0,0 +1,15 @@
+let emilysmap = L.map('MAP').setView([48.858958, 2.349533], 12)
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png').addTo(emilysmap)
+let mymarker = L.marker([48.858273, 2.294498]).addTo(emilysmap)
+let mypolygon = L.polygon([
+  [48.870392,	2.276044],
+  [48.872819,	2.292438],
+  [48.864463,	2.286773]
+]).addTo(emilysmap);
+
+mypolygon.bindPopup('L\'Arc de Triomphe de l\'Etoile')
+mymarker.bindPopup('Eiffel Tower')
+
+emilysmap.on('click', function (event) {
+  console.log('You clicked the map at ' + event.latlng)
+})
