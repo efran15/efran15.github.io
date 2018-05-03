fa713470f53338cdbc5883fe3c0e5ef1012fa445
efran15 <35343406+efran15@users.noreply.github.com>
2018-05-03 15:24:25 -0500

    update

---
+++ Visit England/UKdata.js
@@ -0,0 +1,312 @@
+let UKdata =
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "Buckingham Palace",
+        "Image": "buckingham-palace.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1423501968383789,
+          51.50114942200583
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "The Roman Baths",
+        "Image": "the-roman-baths.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -2.359732389450073,
+          51.381059003075926
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "Hadrian's Wall",
+        "Image": "hadrians-wall.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -2.603183090686798,
+          54.990038599237636
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "Westminster Abbey",
+        "Image": "westminster-abbey.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1273566484451294,
+          51.49935949819495
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "The British Museum",
+        "Image": "the-british-museum.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.12732982635498047,
+          51.519492088136865
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "Shakespeare's Globe",
+        "Image": "globe.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.09713351726531982,
+          51.50809305988694
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "Tower of London",
+        "Image": "tower-of-london.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.07613182067871092,
+          51.50822160488022
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "Big Ben",
+        "Image": "big-ben.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.12464761734008789,
+          51.500681911375345
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "Stonehenge",
+        "Image": "stonehenge.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -1.8261122703552244,
+          51.17880491665441
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "Kensington Palace",
+        "Image": "kensington-palace.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.18822669982910156,
+          51.50518984975107
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "Windsor Castle",
+        "Image": "windsor-castle.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.6040871143341064,
+          51.48395523392852
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "St. Paul's Cathedral",
+        "Image": "st-pauls-cathedral.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.09846925735473633,
+          51.51376371940495
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "London Eye",
+        "Image": "london-eye.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1196211576461792,
+          51.50329656994836
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "Tower Bridge",
+        "Image": "tower-bridge.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.07519841194152832,
+          51.50570406031107
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "Trafalgar Square",
+        "Image": "trafalgar-square.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.12801647186279297,
+          51.50801459718032
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "Piccadilly Circus",
+        "Image": "piccadilly-circus.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.13458251953125,
+          51.50987095521003
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "Harrods",
+        "Image": "harrods.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.16292810440063477,
+          51.49931942446928
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Name": "The National Gallery",
+        "Image": "national-gallery.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.12860655784606934,
+          51.50889604279776
+        ]
+      }
+    }
+  ]
+}
---
+++ Visit England/UKmap.js
@@ -0,0 +1,50 @@
+let emilysmap = L.map('MAP').setView([52.582096,	-1.470629], 6)
+
+let emilysmap1 = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png').addTo(emilysmap)
+
+let emilysmap2 = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}')
+
+let emilysmap3 = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}')
+
+let emilysBasemaps = {
+  'Open street map': emilysmap1,
+  'Open top map': emilysmap2,
+  'World Physical': emilysmap3,
+}
+
+function createicon (feature, latlng) {
+  let myIcon = L.icon({
+    iconUrl: 'icon.jpg',
+    iconSize:     [60, 60], // width and height of the image in pixels
+    iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
+    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+  })
+  return L.marker(latlng, { icon: myIcon })
+}
+
+
+let myIconOptions = {
+  pointToLayer: createicon
+}
+
+
+emilysgeoJSONstyle = function (attraction) {
+  let Name = attraction.properties.Name
+
+  }
+
+function createPopup (attraction, attractionlayer) {
+  let Name = attraction.properties.Name
+  let filename = attraction.properties.Image
+  let image = '<img class="popupimage" src="Image/'+ filename +'" alt="popupimage">'
+  attractionlayer.bindPopup( Name + '<br>' + image)
+}
+
+emilysgeoJSONoptions = {
+  style: emilysgeoJSONstyle,
+  onEachFeature: createPopup,
+  pointToLayer: createicon
+}
+L.geoJSON(UKdata, emilysgeoJSONoptions).addTo(emilysmap)
+
+L.control.layers(emilysBasemaps).addTo(emilysmap)
